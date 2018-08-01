c42ec8e8c465c3db08cd6950975e7eea840600ee
hrai <rai.hangjit@gmail.com>
2018-08-01 21:03:32 +1000

    Created Move_git_hooks.sh

---
+++ move_git_hooks.sh
@@ -0,0 +1,12 @@
+echo 'Moving prepare-commit-msg to .global-git-hooks directory....'
+mkdir -p ~/.global-git-hooks
+cp prepare-commit-msg ~/.global-git-hooks/prepare-commit-msg
+echo 'Successfully moved prepare-commit-msg to .global-git-hooks directory....'
+
+echo 'Setting the global git-hooks folder to ~/.global-git-hooks'
+git config --global core.hooksPath ~/.global-git-hooks
+echo 'Successfully set the global git-hooks folder to ~/.global-git-hooks'
+
+echo -e "\n"
+
+read -n 1 -s -r -p "Press any key to continue..."
