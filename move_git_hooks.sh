1e391234916b55188f9296d525096e203a4e7813
Hangjit Rai <hangjit.rai@adslot.com>
2018-08-02 09:43:53 +1000

    Quick Updates

--- move_git_hooks.sh
+++ move_git_hooks.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.global-git-hooks
 cp prepare-commit-msg ~/.global-git-hooks/prepare-commit-msg
 echo 'Successfully moved prepare-commit-msg to .global-git-hooks directory....'
 
-echo 'Setting the global git-hooks folder to ~/.global-git-hooks'
+echo -e '\nSetting the global git-hooks folder to ~/.global-git-hooks'
 git config --global core.hooksPath ~/.global-git-hooks
 echo 'Successfully set the global git-hooks folder to ~/.global-git-hooks'
 
