e799bc166c9fb8869acc4076d106697d523d59d6
hrai <rai.hangjit@gmail.com>
2018-08-01 21:05:18 +1000

    Added Key Press Listener

--- move_bashrc.sh
+++ move_bashrc.sh
@@ -1,3 +1,7 @@
 echo 'Moving .bashrc to home directory....'
 cp .bashrc ~/.bashrc
 echo 'Successfully moved .bashrc to home directory....'
+
+echo -e "\n"
+
+read -n 1 -s -r -p "Press any key to continue..."
