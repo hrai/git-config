9047c0366ce6e83d5e849998e16754acb904179e
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-06 14:17:53 +1000

    added zrc mover

---
+++ move_zrc.sh
@@ -0,0 +1,7 @@
+echo 'Moving .zrc to home directory....'
+cp .bashrc ~/.zrc
+echo 'Successfully moved .zrc to home directory....'
+
+echo -e "\n"
+
+read -n 1 -s -r -p "Press any key to continue..."
