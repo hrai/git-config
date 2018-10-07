209ffeba593820ded74c9380f98b37b1f906358b
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-07 12:23:07 +1100

    Added Tmux.conf File

---
+++ move_tmux_conf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo 'Moving .tmux.conf to home directory....'
+cp .tmux.conf ~/.tmux.conf
+echo 'Successfully moved .tmux.conf to home directory....'
+
+echo -e "\n"
+
+read -n 1 -s -r -p "Press any key to continue..."
