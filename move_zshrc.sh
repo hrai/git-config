be76665f1bd28029d5aa3c824f08f625f3db0e30
hrai <rai.hangjit@gmail.com>
2018-10-09 21:58:59 +1100

    Added Git Open Plugin

--- move_zshrc.sh
+++ move_zshrc.sh
@@ -12,6 +12,7 @@ echo "Successfully moved .zshrc to home directory...."
 echo "Cloning plugins to $ZSH_CUSTOM...."
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/paulirish/git-open.git $ZSH_CUSTOM/plugins/git-open
 echo "Successfully cloned plugins to $ZSH_CUSTOM...."
 
 echo -e "\n"
