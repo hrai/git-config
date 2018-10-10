e05839ffd0d6daea1950906fd34519d4a63db79d
Hangjit Rai <hangjit.rai@ofx.com>
2018-10-10 11:47:25 +1100

    Added Package Install Script

---
+++ 0_install_packages.sh
@@ -0,0 +1,18 @@
+echo ''
+echo 'Installing apps....'
+
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Update 0_install_packages.sh file to include the packages to install."
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sudo apt install git
+    sudo apt install python3
+    sudo apt install zsh
+
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Do something under 32 bits Windows NT platform
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Do something under 64 bits Windows NT platform
+fi
+
+echo ''
+echo "Successfully moved dot files to home directory...."
