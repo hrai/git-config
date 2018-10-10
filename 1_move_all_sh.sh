8465ea07ec5cc1ac762a35193387aff579130400
Hangjit Rai <hangjit.rai@ofx.com>
2018-10-10 11:42:57 +1100

    Modified File Names

--- 1_move_all_sh.sh
+++ 1_move_all_sh.sh
@@ -1,17 +1,17 @@
 echo 'Moving bashrc to home directory....'
-sh move_bashrc.sh
+sh 2_move_bashrc.sh
 
 echo ''
 echo 'Moving git hooks to home directory....'
-sh move_git_hooks.sh
+sh 5_move_git_hooks.sh
 
 echo ''
 echo 'Moving tmux config to home directory....'
-sh move_tmux_conf.sh
+sh 4_move_tmux_conf.sh
 
 echo ''
 echo 'Moving zshrc to home directory....'
-sh move_zshrc.sh
+sh 3_move_zshrc.sh
 
 echo ''
 echo "Successfully moved dot files to home directory...."
