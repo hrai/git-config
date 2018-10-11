9895166f673a0002bf286be3c67afcd66f8769c6
Hangjit Rai <hangjit.rai@ofx.com>
2018-10-11 13:23:01 +1100

    Converted Sh To Bash

--- 1_move_all_sh.sh
+++ 1_move_all_sh.sh
@@ -1,17 +1,17 @@
 echo 'Moving bashrc to home directory....'
-sh 2_move_bashrc.sh
+bash 2_move_bashrc.sh
 
 echo ''
 echo 'Moving git hooks to home directory....'
-sh 5_move_git_hooks.sh
+bash 5_move_git_hooks.sh
 
 echo ''
 echo 'Moving tmux config to home directory....'
-sh 4_move_tmux_conf.sh
+bash 4_move_tmux_conf.sh
 
 echo ''
 echo 'Moving zshrc to home directory....'
-sh 3_move_zshrc.sh
+bash 3_move_zshrc.sh
 
 echo ''
 echo "Successfully moved dot files to home directory...."
