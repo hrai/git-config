82734f8b58841a839e8b6e169a691d63834cf636
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-17 15:08:11 +1100

    added vim installer script

---
+++ 6_set_vim_configs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'Moving vim-files...'
+
+ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/hrai/vim-files/master/refresh_and_install_vimrcs.sh | sh
+
+echo "Successfully moved vim-files to ~/.vim_runtime"
+fi
+
+echo -e "\n"
+
+read -n 1 -s -r -p "Press any key to continue..."
