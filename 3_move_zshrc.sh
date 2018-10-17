7a9c07cd27e4060055818215cd09cff625832d22
Hangjit Rai <rai.hangjit@gmail.com>
2018-10-17 15:48:36 +1100

    securing dirs

--- 3_move_zshrc.sh
+++ 3_move_zshrc.sh
@@ -6,6 +6,11 @@ if hash zsh 2>/dev/null; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
     echo 'Successfully cloned zplugin....'
 
+    # securing the directories
+    compaudit | xargs chmod g-w
+    # sudo chmod -R 777 ~/.zplugin
+    # sudo chown -R root:$USER ./.zplugin
+
     echo 'Moving .zshrc to home directory....'
     cp .zshrc ~/.zshrc
     cat .bashrc >> ~/.zshrc
