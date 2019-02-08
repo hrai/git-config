7ff7e28948dbee834f95995c6c78ab1409eaa0f4
Hangjit Rai <rai.hangjit@gmail.com>
2019-02-08 17:21:16 +1100

    Updated Edit Command Line Widget Keybinding

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -66,7 +66,7 @@ fi
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line
+bindkey '^x^e' edit-command-line
 
 # file rename magick
 bindkey "^[m" copy-prev-shell-word
