05ab2ad00c994b6357f0b41e71daedcf73dbbca9
Hangjit Rai <rai.hangjit@gmail.com>
2019-05-01 21:16:44 +1000

    File Rename And Cleanup

---
+++ 1_move_all_dot_files.sh
@@ -0,0 +1,25 @@
+echo 'Moving bashrc to home directory....'
+bash 2_move_bashrc.sh
+
+echo ''
+# Moving git hooks to home directory....
+bash 5_move_git_hooks.sh
+
+echo ''
+# Moving tmux config to home directory....
+bash 4_move_tmux_conf.sh
+
+echo ''
+# Moving zshrc to home directory....
+bash 3_move_zshrc.sh
+
+echo ''
+# Moving vim-files to home directory....
+bash 6_set_vim_configs.sh
+
+echo ''
+# Installing YCM....
+bash 8_move_editorconfig.sh
+
+echo ''
+echo "Successfully moved dot files to home directory...."
