664e9913980578bd38cc7fcbb5f614d8ff4531fa
Hangjit Rai <rai.hangjit@gmail.com>
2019-05-15 17:29:15 +1000

    Added Nerdfont Hack Install Script For Ubuntu

--- 0_install_packages.sh
+++ 0_install_packages.sh
@@ -28,6 +28,12 @@ function install_package_mac_cask () {
   fi
 }
 
+function install_hack_nerd_font() {
+    mkdir -p ~/.local/share/fonts
+    cd ~/.local/share/fonts && curl -fLo "Hack Regular Nerd Font Complete.otf"
+    https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts%2FHack%2FRegular%2Fcomplete%2FHack%20Regular%20Nerd%20Font%20Complete.ttf
+}
+
 function install_aws_cli() {
     pip3 install awscli --upgrade --user
 }
@@ -117,6 +123,8 @@ function install_apps() {
       sudo pip3 install thefuck
       install_ctags
       install_aws_cli
+
+      install_hack_nerd_font
   fi
 
   echo 'Finished installing apps....'
