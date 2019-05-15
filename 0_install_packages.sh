bad34a4568e9ed84513cf1f0806ed4193371b0f1
Hangjit Rai <rai.hangjit@gmail.com>
2019-05-15 17:31:58 +1000

    Disabling Powerline Installer

--- 0_install_packages.sh
+++ 0_install_packages.sh
@@ -99,7 +99,7 @@ function install_apps() {
         install_package "curl"
         install_package "dos2unix"
         install_package "editorconfig"
-        install_package "fonts-powerline"
+        # install_package "fonts-powerline"
         # install_package "fonts-hack-ttf"
         install_package "git"
         install_package "git-extras"
