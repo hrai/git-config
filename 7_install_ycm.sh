410c876fb3b4bc152d247e079e6b20732ad7b0d2
Hangjit Rai <rai.hangjit@gmail.com>
2019-06-30 10:56:59 +1000

    Added Js Autocomplete Via TS Server

--- 7_install_ycm.sh
+++ 7_install_ycm.sh
@@ -3,7 +3,7 @@ sudo apt install build-essential cmake python3-dev python-dev npm -y
 
 cd ~/.vim/plugged/YouCompleteMe
 
-install_mono() {
+setup_cs_autocomplete() {
     wget http://download.mono-project.com/repo/xamarin.gpg
     sudo apt-key add xamarin.gpg -y
     echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee --append -y
@@ -15,9 +15,14 @@ install_mono() {
     sudo certmgr -ssl -m https://nugetgallery.blob.core.windows.net -y
     sudo certmgr -ssl -m https://nuget.org -y
     mozroots --import --sync -y
+
+    sudo ./install.py --cs-completer
 }
 
-install_mono
+setup_js_autocomplete() {
+    sudo npm install -g typescript
+}
+
 
 # The following additional language support options are available:
 #     C# support: install Mono and add --cs-completer when calling install.py.
@@ -25,5 +30,6 @@ install_mono
 #     JavaScript and TypeScript support: install Node.js and npm and add --ts-completer when calling install.py.
 #     Rust support: install Rust and add --rust-completer when calling install.py.
 #     Java support: install JDK8 (version 8 required) and add --java-completer when calling install.py.
-sudo ./install.py --ts-completer
-sudo ./install.py --cs-completer
+
+setup_js_autocomplete
+setup_cs_autocomplete
