2f1d0407790fa854454994e126f982661ec574e2
Hangjit Rai <rai.hangjit@gmail.com>
2020-01-10 15:18:01 +1100

    Updating Gitconfig

--- 5_move_git_hooks.sh
+++ 5_move_git_hooks.sh
@@ -33,7 +33,6 @@ git config --global user.email rai.hangjit@gmail.com
 git config --global user.name "Hangjit Rai"
 git config --global init.templatedir '~/.global-git-hooks'
 
-
 function is_windows() {
     local SYSTEM_NAME="$(expr substr $(uname -s) 1 10)"
 
@@ -59,9 +58,10 @@ read user_response
 if [ "$user_response" = "y" ]
 then
     cp .gitignore_global ~/.gitignore
-    git config --global core.excludesfile '~/.gitignore'
     echo "Successfully moved .gitignore file to ~/.gitignore"
 fi
+
+git config --global core.excludesfile '~/.gitignore'
 
 echo -e "\n"
 
