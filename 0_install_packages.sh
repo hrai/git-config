f02d3c73339764f2719b2f5956647f3829db899c
Hangjit Rai <rai.hangjit@gmail.com>
2020-03-13 20:39:06 +1100

    Cleanup

--- 0_install_packages.sh
+++ 0_install_packages.sh
@@ -43,8 +43,7 @@ function install_docker() {
 }
 
 function install_dotnet() {
-    wget -q https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O
-    packages-microsoft-prod.deb
+    wget -q https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
 
     sudo apt-get update
