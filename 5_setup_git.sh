db824b79f2adaca4dfc75a7d1a52cfbe3862d02f
Hangjit Rai <hangjit.rai@outlook.com>
2020-05-10 09:58:04 +1000

    Added Gitattributes

--- 5_setup_git.sh
+++ 5_setup_git.sh
@@ -15,6 +15,18 @@ function create_ssh_key() {
     fi
 }
 
+function is_windows() {
+    local SYSTEM_NAME="$(expr substr $(uname -s) 1 10)"
+
+    if [ "$SYSTEM_NAME" = "MINGW64_NT" ]; then
+        true
+    elif [ "$SYSTEM_NAME" = "MINGW32_NT" ]; then
+        true
+    else
+        false
+    fi
+}
+
 create_ssh_key
 
 echo 'Moving git hooks to .global-git-hooks directory....'
@@ -27,6 +39,12 @@ else
 fi
 
 cp git-hooks/pre-commit ~/.global-git-hooks/hooks/pre-commit
+
+cp .gitattributes ~/
+echo "Successfully moved .gitattributes file to ~/.gitignore"
+
+cp .gitignore_global ~/.gitignore
+echo "Successfully moved .gitignore file to ~/.gitignore"
 
 chmod 777 ~/.global-git-hooks/hooks/prepare-commit-msg
 chmod 777 ~/.global-git-hooks/hooks/pre-commit
@@ -49,18 +67,7 @@ git config --global merge.tool kdiff3
 git config --global user.email hangjit.rai@outlook.com
 git config --global user.name "Hangjit Rai"
 git config --global init.templatedir '~/.global-git-hooks'
-
-function is_windows() {
-    local SYSTEM_NAME="$(expr substr $(uname -s) 1 10)"
-
-    if [ "$SYSTEM_NAME" = "MINGW64_NT" ]; then
-        true
-    elif [ "$SYSTEM_NAME" = "MINGW32_NT" ]; then
-        true
-    else
-        false
-    fi
-}
+git config --global core.attributesfile '~/.gitattributes'
 
 if is_windows; then
     git config --global core.autocrlf true
@@ -69,9 +76,6 @@ else
     git config --global core.autocrlf input
     git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 fi
-
-cp .gitignore_global ~/.gitignore
-echo "Successfully moved .gitignore file to ~/.gitignore"
 
 git config --global core.excludesfile '~/.gitignore'
 
