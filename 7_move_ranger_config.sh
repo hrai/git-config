931758be15b67263558756b4fac03ba632d74fa5
Hangjit Rai <hangjit.rai@outlook.com>
2020-09-29 11:03:39 +1000

    Added Ranger Config

---
+++ 7_move_ranger_config.sh
@@ -0,0 +1,7 @@
+echo 'Moving ranger config to home directory....'
+
+git clone https://github.com/cdump/ranger-devicons2 ~/.config/ranger/plugins/devicons2
+cp ranger/* ~/.config/ranger
+
+echo "Successfully moved ranger config to home directory...."
+
