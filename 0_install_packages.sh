5b11cc6275c9f5f3366d6d01d9b920237564cd50
Hangjit Rai <hangjit.rai@outlook.com>
2020-11-18 09:24:34 +1100

    Installing Dn 5\.0

--- 0_install_packages.sh
+++ 0_install_packages.sh
@@ -46,10 +46,10 @@ function install_dotnet() {
     wget -q https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
     sudo dpkg -i packages-microsoft-prod.deb
 
-    sudo apt-get update
-    sudo apt-get install apt-transport-https
-    sudo apt-get update
-    sudo apt-get install dotnet-sdk-3.1
+    sudo apt-get update; \
+      sudo apt-get install -y apt-transport-https && \
+      sudo apt-get update && \
+      sudo apt-get install -y dotnet-sdk-5.0
 }
 
 function install_ctags() {
